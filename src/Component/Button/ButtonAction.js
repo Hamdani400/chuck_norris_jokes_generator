4e375c30ff174eb381b9fddd8668c423313fc148
Hamdani Dinul Haq <67686352+Hamdani400@users.noreply.github.com>
2021-10-11 09:58:45 +0700

    Update ButtonAction.js
    add random btn

--- src/Component/Button/ButtonAction.js
+++ src/Component/Button/ButtonAction.js
@@ -4,16 +4,29 @@ import {Link} from 'react-router-dom';
 import './ButtonAction.scss';
 
 export default function ButtonAction (props) {
+  function onClickHandler () {
+    props.onClick && props.onClick ();
+  }
+
   return (
     <div>
-      <Link
-        to={props.href || '/'}
-        style={{width: props.width}}
-        type="button"
-        className="btn"
-      >
-        {props.text}
-      </Link>
+      {!props.isGetRandomJoke
+        ? <Link
+            to={props.href || '/'}
+            style={{width: props.width}}
+            type="button"
+            className="btn"
+          >
+            {props.text}
+          </Link>
+        : <button
+            onClick={onClickHandler}
+            style={{width: props.width}}
+            type="button"
+            className="btn"
+          >
+            {props.text}
+          </button>}
     </div>
   );
 }
