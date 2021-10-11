71362812a30133e5ca227d690f11e988d302099b
Hamdani Dinul Haq <67686352+Hamdani400@users.noreply.github.com>
2021-10-11 10:18:34 +0700

    Update LandingPage.js

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -3,6 +3,11 @@ import Header from '../../Parts/Header/Header.js';
 import Body from '../../Parts/Body/Body.js';
 
 export default function LandingPage () {
+  useEffect (() => {
+    document.title = 'Chuck Norris Jokes | Home';
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  
   return (
     <div>
       <Header />
